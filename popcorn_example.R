957e0e101eb251363d353272ddc42a3cb709f349
Fumi <fumihiko@takeuchi.name>
2018-03-15 16:04:23 +0900

    Minor changes of terminology in popcorn_example.R

--- popcorn_example.R
+++ popcorn_example.R
@@ -35,11 +35,11 @@ wkeep = 1/pmax(1,
                (dataunweighted$score1[match(data$id, dataunweighted$id)])[keep]);
 # The computation below took 34 minutes on a 24 core machine.
 # Estimated values should be
-# heritability                     = 0.06630128
-# nu for t-distribution            = 3.38048012
-# constant for LD score regression = 0.98506231
+# heritability                      = 0.06630128
+# nu for t-distribution             = 3.38048012
+# intercept for LD score regression = 0.98506231
 optim(c(0.5, 2.5, 1),
-      nll_onepop_snpt_envnorm_const,
+      nll_onepop_snpt_envnorm_intercept,
       lower=c(0.01, 2.01, 0.5),
       upper=c(0.99, 10, 1.5),
       method="L-BFGS-B");
@@ -54,11 +54,11 @@ wkeep = 1/pmax(1,
                (dataunweighted$score2[match(data$id, dataunweighted$id)])[keep]);
 # The computation below took 59 minutes on a 24 core machine.
 # Estimated values should be
-# heritability                     = 0.1180501
-# nu for t-distribution            = 2.8696062
-# constant for LD score regression = 0.9958312
+# heritability                      = 0.1180501
+# nu for t-distribution             = 2.8696062
+# intercept for LD score regression = 0.9958312
 optim(c(0.5, 2.5, 1),
-      nll_onepop_snpt_envnorm_const,
+      nll_onepop_snpt_envnorm_intercept,
       lower=c(0.01, 2.01, 0.5),
       upper=c(0.99, 10, 1.5),
       method="L-BFGS-B");
@@ -83,9 +83,9 @@ wkeep = 1/pmax(1,
 optim(c(0, 5),
       h1 = 0.06630128, # heritability
       h2 = 0.1180501,
-      c1 = 0.98506231, # constant for LD score regression
+      c1 = 0.98506231, # intercept for LD score regression
       c2 = 0.9958312,
-      nll_twopop_snpt_envnorm_const,
+      nll_twopop_snpt_envnorm_intercept,
       lower=c(-0.99, 2.01),
       upper=c(0.99, 10),
       method="L-BFGS-B");
